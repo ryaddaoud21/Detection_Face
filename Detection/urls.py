18879d34e5db85b34cbae1dc56dbd7f3ed593b6b
Your Name <you@example.com>
2023-03-20 14:53:30 +0100

    hands

--- Detection/urls.py
+++ Detection/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index,webcam_feed
+from .views import *
 
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('video_feed/', webcam_feed, name='video_feed'),
+    path('hand/', hand, name='hand'),
+    path('video_feed1/', webcam_feed1, name='video_feed1'),
+    path('video_feed2/', webcam_feed2, name='video_feed2'),
 
 ]
--- Detection/views.py
+++ Detection/views.py
@@ -20,7 +20,7 @@ def detect_faces():
         # Draw a rectangle around each face
         for (x, y, w, h) in faces:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, "You are ugly", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(frame, "Your face ", (x, y + h + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 
         # Convert the frame to JPEG format
@@ -34,9 +34,62 @@ def detect_faces():
 
 # Function to stream the video feed from the webcam
 @gzip.gzip_page
-def webcam_feed(request):
+def webcam_feed1(request):
     return StreamingHttpResponse(detect_faces(), content_type='multipart/x-mixed-replace; boundary=frame')
+
+@gzip.gzip_page
+def webcam_feed2(request):
+    return StreamingHttpResponse(detect_handes(), content_type='multipart/x-mixed-replace; boundary=frame')
 
 # Function to render the index.html template
 def index(request):
     return render(request, 'Detection/index.html')
+def hand(request):
+    return render(request, 'Detection/index_.html')
+
+
+import cv2
+import mediapipe as mp
+import time
+
+
+def detect_handes():
+
+    cap = cv2.VideoCapture(0)
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands(static_image_mode=False,
+                          max_num_hands=2,
+                          min_detection_confidence=0.5,
+                          min_tracking_confidence=0.5)
+    mpDraw = mp.solutions.drawing_utils
+
+    pTime = 0
+    cTime = 0
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        #print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+                for id, lm in enumerate(handLms.landmark):
+                    #print(id,lm)
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x *w), int(lm.y*h)
+                    #if id ==0:
+                    cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+
+                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+        cTime = time.time()
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
